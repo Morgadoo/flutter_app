bb59d6eaa8fcb7444f5708d4a96a107e7be722ad
Morgadoo <luismorgado6@gmail.com>
2020-06-17 01:18:06 +0100

    test flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flare_flutter/flare_actor.dart';
-
+import 'package:flutter_app/constants.dart';
+import 'dart:math';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 
 void main() {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Curso APP',
+      theme: ThemeData(),
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -22,27 +27,49 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
+
+
+
 class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Center(
-            child: FlareActor('assets/Cosmos.flr', animation: 'Untitled')
-          ),
-          decoration: BoxDecoration(
-          gradient: LinearGradient(
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
-          colors: [Colors.purple, Colors.blue])
-          ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, top: 40, right:20),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SvgPicture.asset('assets/icons/menu.svg',height: 30,width: 30,),
+
+                new Container(
+                  width: 60,
+                  height: 60,
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: new DecorationImage(
+                      image: new NetworkImage("https://api.adorable.io/avatars/100/"+ random().toString())
+                    )
+                  )
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Text('Olá Luís', style: kHeadingextStyle),
+            Text('Encontra um curso a tua medida', style: kSubheadingextStyle),
+          ],
         ),
+      ),
     );
   }
+}
 
 
-
-
+int random(){
+  var rng = new Random();
+  var l = new List.generate(1, (_) => rng.nextInt(400));
+  return l[0];
 }
